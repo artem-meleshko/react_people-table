e52e5c6465f3b1e43d960a0b64ab9febefa7c661
artem-meleshko <timonlondon@hotmail.com>
2019-07-14 19:36:35 +0300

    Updates

---
+++ precache-manifest.0c09db0bf5deecd909195f4edd1baf03.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fce103d9d982b31215f1ab0b585a19df",
+    "url": "/react_people-table/index.html"
+  },
+  {
+    "revision": "c0a3c09ea6ce466ca699",
+    "url": "/react_people-table/static/css/main.04328f61.chunk.css"
+  },
+  {
+    "revision": "17bdb5ce1362e72c08ed",
+    "url": "/react_people-table/static/js/2.d9050a23.chunk.js"
+  },
+  {
+    "revision": "c0a3c09ea6ce466ca699",
+    "url": "/react_people-table/static/js/main.1dd9b45c.chunk.js"
+  },
+  {
+    "revision": "c6b2e506936d419d6d52",
+    "url": "/react_people-table/static/js/runtime~main.b2b73302.js"
+  }
+]);
